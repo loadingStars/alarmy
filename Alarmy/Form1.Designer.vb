a7b4402735b2817c3f4219141e44ca9d73d529b7
loadingStars <32369271+loadingStars@users.noreply.github.com>
2017-09-28 20:03:26 +0200

    Added time functionality

--- Alarmy/Form1.Designer.vb
+++ Alarmy/Form1.Designer.vb
@@ -22,17 +22,37 @@ Partial Class Form1
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblCurrentTime = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
+        '
+        'lblCurrentTime
+        '
+        Me.lblCurrentTime.AutoSize = True
+        Me.lblCurrentTime.Font = New System.Drawing.Font("Microsoft Sans Serif", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCurrentTime.Location = New System.Drawing.Point(12, 9)
+        Me.lblCurrentTime.Name = "lblCurrentTime"
+        Me.lblCurrentTime.Size = New System.Drawing.Size(232, 76)
+        Me.lblCurrentTime.TabIndex = 0
+        Me.lblCurrentTime.Text = "Label1"
+        '
+        'Timer1
+        '
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(426, 313)
+        Me.ClientSize = New System.Drawing.Size(498, 374)
+        Me.Controls.Add(Me.lblCurrentTime)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Alarmy"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents lblCurrentTime As Label
+    Friend WithEvents Timer1 As Timer
 End Class
--- Alarmy/Form1.vb
+++ Alarmy/Form1.vb
@@ -4,6 +4,19 @@
 
 Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = True
+
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim currentTime As String
+        currentTime = Now.Hour.ToString + ":" + Now.Minute.ToString + ":" + Now.Second.ToString
+
+
+        lblCurrentTime.Text = currentTime
+
 
     End Sub
 End Class
