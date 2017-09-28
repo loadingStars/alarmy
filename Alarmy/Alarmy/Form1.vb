4eee84464d22e4e12769f67aaaf771732c1eb591
loadingStars <32369271+loadingStars@users.noreply.github.com>
2017-09-28 19:41:28 +0200

    Let's get started!
    Started with the files!

---
+++ Alarmy/Alarmy/Form1.vb
@@ -0,0 +1,9 @@
+﻿'Hello and welcom to Alarmy
+'a very simple alarm clock
+'for your desktop :)
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Do nothing, just see if the push works!
+    End Sub
+End Class
