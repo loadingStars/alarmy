07026206641dcee28cd2a4d599dc6e5f06674970
loadingStars <32369271+loadingStars@users.noreply.github.com>
2017-09-28 20:35:58 +0200

    Added 0 for single number seconds

--- Alarmy/Form1.vb
+++ Alarmy/Form1.vb
@@ -17,7 +17,20 @@ Public Class Form1
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Dim currentTime As String
-        currentTime = Now.Hour.ToString + ":" + Now.Minute.ToString + ":" + Now.Second.ToString
+        Dim hour As String
+        Dim minute As String
+        Dim second As String
+
+        hour = Now.Hour.ToString
+        minute = Now.Minute.ToString
+        second = Now.Second.ToString
+
+        If hour.Length = 1 Then hour = "0" + hour
+        If minute.Length = 1 Then minute = "0" + minute
+        If second.Length = 1 Then second = "0" + second
+
+
+        currentTime = hour + ":" + minute + ":" + second
 
 
         lblCurrentTime.Text = currentTime
